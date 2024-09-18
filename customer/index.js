600d0e615854868b3ec242abe99014bfa1fc6246
Marc Nelson B. Ochavo <89735506+n3wt0n03@users.noreply.github.com>
2024-09-18 18:56:29 +0800

    added post and delete for customer

--- customer/index.js
+++ customer/index.js
@@ -6,50 +6,79 @@ app.use(express.json());
 
 let customers = [];
 
-app.get('/customers/getCustomer/:customerId', (req,res) => {
+app.get('/customers/getCustomer/:customerId', (req, res) => {
+  const customerId = parseInt(req.params.customerId);
+  const customer = customers.find((customer) => customer.id === customerId);
 
-    const customerId = parseInt(req.params.customerId);
-    const customer = customers.find((customer) => customer.id === customerId);
-    
-    if(!customer){
-        return res.status(404).json({error: "Customer not found"});
-    }
-    try{
-        res.status(200).json(customer);
-    } catch (error) {
-        res.status(500).json({error: "There is an error"});
-    }
-})
+  if (!customer) {
+    return res.status(404).json({ error: 'Customer not found' });
+  }
+  try {
+    res.status(200).json(customer);
+  } catch (error) {
+    res.status(500).json({ error: 'There is an error' });
+  }
+});
 
+app.post('/customers/customerAdd', (req, res) => {
+  const firstName = req.body.firstName;
 
-app.post('/customers/customerAdd', (req,res) => {
-    
-    const firstName = req.body.firstName;
+  const customer = {
+    id: customers.length,
+    firstName,
+  };
 
-    const customer = {
-        id: customers.length,
-        firstName
-    }
+  try {
+    customers.push(customer);
+    res.status(201).json(customers);
+  } catch (error) {
+    res.status(500).json({ error: 'There is an error' });
+    console.log(error);
+  }
+});
 
-    try{
-        customers.push(customer);
-        res.status(201).json(customers);
-    } catch (error){
-        res.status(500).json({error: "There is an error"});
-        console.log(error);
-    }
+app.put('/customers/customerUpdate/:customerId', (req, res) => {
+  const customerId = parseInt(req.params.customerId);
+  const customerIndex = customers.findIndex(
+    (customer) => customer.id === customerId
+  );
 
+  if (customerIndex === -1) {
+    return res.status(404).json({ error: 'Customer not found' });
+  }
 
+  const updateCustomer = req.body;
+  customers[customerIndex].firstName = updateCustomer.firstName;
 
-})
+  try {
+    res.status(200).json(updateCustomer);
+  } catch (error) {
+    res.status(500).json({ error: 'There is an error' });
+  }
+});
 
+app.delete('/customers/customerDelete/:customerId', (req, res) => {
+  if (customers.length === 0) {
+    return res.status(404).json({ error: 'No customers found' });
+  }
 
+  const customerId = parseInt(req.params.customerId);
+  const customerIndex = customers.findIndex(
+    (customer) => customer.id === customerId
+  );
 
+  if (customerIndex === -1) {
+    return res.status(404).json({ error: 'Customer not found' });
+  }
 
-
-
-
+  try {
+    customers.splice(customerIndex, 1);
+    res.status(200).json({ message: 'Customer deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'There is an error' });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Customers Server running at port ${port}`);
-})
+  console.log(`Customers Server running at port ${port}`);
+});
